5556dc26436003acd29bcddace1b93dff1e9e109
Gabriel Fairbairn <97549201+NerdSlayer0@users.noreply.github.com>
2022-09-28 15:38:53 -0700

    added Procfile for Heroku hosting

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require("express") ();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
     res.send("Welcome to our SAP Hackathon App!");

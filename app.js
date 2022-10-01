fbc1ea795c4189cc6958b2c4819841c8bb25c72c
RaphaelYun <raphael.yun@gmail.com>
2022-10-01 10:22:02 -0700

    testing mysql pool

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const app = require("express") ();
 const mysql = require("mysql2");
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: "us-cdbr-east-06.cleardb.net",
     port: 3306,
     user: "ba078f4cff050a",
     password: "2ac694ea",
-    database: "heroku_8afbad23634f9c6",
+    database: "heroku_8afbad23634f9c6"
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
     res.send("Welcome to our SAP Hackathon App!");
-    connection.query(`SELECT * FROM customers`, (error, results) => {
-        if (error) console.log(error);
-    });
 });
 
 app.listen(PORT, () => {

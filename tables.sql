4e43f50ac90963d9151498ce9c042e7212565125
Gabriel Fairbairn <97549201+NerdSlayer0@users.noreply.github.com>
2022-10-01 14:55:18 -0700

    changed sql items and added form to event booking page

--- tables.sql
+++ tables.sql
@@ -6,6 +6,7 @@ CREATE TABLE users (
   password VARCHAR(50),
   is_admin TINYINT(1),
   office_location VARCHAR(50),
+  interests VARCHAR(50),
   PRIMARY KEY (ID)
 );
 
@@ -18,15 +19,16 @@ CREATE TABLE events (
   booking_day date,
   booking_time time,
   event_type VARCHAR(50),
-  attendee_list[], 
+  attendee_list VARCHAR(100), 
   PRIMARY KEY (word_ID),
-  FOREIGN KEY (host_ID) REFERENCES users(ID)
-  FOREIGN KEY (meeting_amenity) REFERENCES amenities
+  FOREIGN KEY (host_ID) REFERENCES users(ID),
+  FOREIGN KEY (meeting_amenity) REFERENCES amenities(amenity_ID),
+  FOREIGN KEY (attendee_list) REFERENCES users(ID)
 );
 
 CREATE TABLE amenities (
     amenity_ID int NOT NULL AUTO_INCREMENT,
-    amennity_name VARCHAR(50) NOT NULL,
+    amenity_name VARCHAR(50) NOT NULL,
     booked BOOLEAN,
 )
 
@@ -35,4 +37,6 @@ INSERT INTO users (user_name, first_name, last_name, password, is_admin, office_
 INSERT INTO users (user_name, first_name, last_name, password, is_admin, office_location) VALUES ('lilnaz', 'Naz', 'Mohammadi', 'asdf', '1', 'Yaletown');
 INSERT INTO users (user_name, first_name, last_name, password, is_admin, office_location) VALUES ('monkey', 'Test', 'Dummy', '0123', '0', 'Bogustown');
 
-INSERT INTO events (event_name, meeting_location, meeting_amenity, host_ID, booking_day, booking_time, event_type) VALUES ('', '', '', );
+INSERT INTO events (event_name, meeting_location, meeting_amenity, host_ID, booking_day, booking_time, event_type) VALUES ('coffee', 'Starbucks - Yaletown', 'None', '1', '2022-02-10', '16:30', 'in-person');
+
+INSERT INTO amenities (amenity_name, booked) VALUES ('Ping Pong Room', 0);

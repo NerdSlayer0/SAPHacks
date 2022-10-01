47c5e020be808182ba94826d86307455dca8796e
Gabriel Fairbairn <97549201+NerdSlayer0@users.noreply.github.com>
2022-10-01 16:02:29 -0700

    added to sql tables

--- tables.sql
+++ tables.sql
@@ -4,33 +4,47 @@ CREATE TABLE users (
   first_name VARCHAR(50),
   last_name VARCHAR(50),
   password VARCHAR(50),
-  is_admin TINYINT(1),
   office_location VARCHAR(50),
-  interests VARCHAR(50),
+  department VARCHAR(50),
+  work_count int,
   PRIMARY KEY (ID)
+  FOREIGN KEY office_location REFERENCES all_locations(location_ID)
 );
 
 CREATE TABLE events (
   event_ID int NOT NULL AUTO_INCREMENT,
-  event_name VARCHAR(50) NOT NULL,
   meeting_location VARCHAR(50),
-  meeting_amenity VARCHAR(50), 
-  host_ID int,
-  booking_day date,
-  booking_time time,
   event_type VARCHAR(50),
-  attendee_list VARCHAR(100), 
-  PRIMARY KEY (word_ID),
-  FOREIGN KEY (host_ID) REFERENCES users(ID),
-  FOREIGN KEY (meeting_amenity) REFERENCES amenities(amenity_ID),
+  attendee_list VARCHAR(100),
+  PRIMARY KEY (event_ID),
+  FOREIGN KEY (meeting_location) REFERENCES all_locations(location_ID),
   FOREIGN KEY (attendee_list) REFERENCES users(ID)
 );
 
-CREATE TABLE amenities (
-    amenity_ID int NOT NULL AUTO_INCREMENT,
-    amenity_name VARCHAR(50) NOT NULL,
-    booked BOOLEAN,
-)
+CREATE TABLE user_interests (
+    user_ID int,
+    interest_ID int,
+    PRIMARY KEY (user_ID, interest_ID)
+);
+
+CREATE TABLE interests (
+    interest_ID int,
+    user_ID int,
+    PRIMARY KEY (interest_ID),
+    FOREIGN_KEY (user_ID) REFERENCES user(ID)
+);
+
+CREATE TABLE all_locations (
+    location_ID int NOT NULL AUTO_INCREMENT,
+    location_name VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE chat_messages (
+    message_ID int NOT NULL, AUTO_INCREMENT,
+    invite_code int NOT NULL, AUTO_INCREMENT,
+    sender VARCHAR(50),
+    FOREIGN KEY sender REFERENCES user(user_name)
+);
 
 INSERT INTO users (user_name, first_name, last_name, password, is_admin, office_location) VALUES ('geb', 'Gabriel', 'Fairbairn', 'asdf', '1', 'Yaletown');
 INSERT INTO users (user_name, first_name, last_name, password, is_admin, office_location) VALUES ('raphael', 'Secret', 'Weapon', 'asdf', '1', 'Yaletown');
@@ -39,4 +53,4 @@ INSERT INTO users (user_name, first_name, last_name, password, is_admin, office_
 
 INSERT INTO events (event_name, meeting_location, meeting_amenity, host_ID, booking_day, booking_time, event_type) VALUES ('coffee', 'Starbucks - Yaletown', 'None', '1', '2022-02-10', '16:30', 'in-person');
 
-INSERT INTO amenities (amenity_name, booked) VALUES ('Ping Pong Room', 0);
+INSERT INTO amenities (amenity_name) VALUES ('Ping Pong Room');

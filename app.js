1706efc6d332d7d39ff11c2a8dcae8ebddd270bb
RaphaelYun <raphael.yun@gmail.com>
2022-10-02 09:10:47 -0700

    animated the bell, stabilized the font link

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ io.use(sharedsession(session));
 io.on("connection", socket => {
     let session = socket.handshake.session;
     socket.on("postChat", (message) => {
-        io.emit("updateChat", session.username + ": " + message);
+        io.emit("updateChat", message, session.username);
         pool.query(`INSERT INTO chat_messages VALUES (NULL, ?, ?, ?)`, [session.room, session.username, message], (error) => {
             if (error) console.log(error);
         });
--- public/js/room.js
+++ public/js/room.js
@@ -1,26 +1,35 @@
 const socket = io();
 
+let count = 0;
+
+const input = document.getElementById("chatContent");
+let last = "";
+
 function sendChat() {
-  const input = document.getElementById("chatContent");
   const message = input.value.trim();
-  if (message) socket.emit("postChat", message);
   input.value = "";
-  const list = document.getElementById("chatList");
-  list.scrollTop = list.scrollHeight;
-}
-
-socket.on("updateChat", (message) => {
-  postChat(message);
-});
-
-function postChat(message) {
-  let template = document.getElementById("newChat").content.cloneNode(true);
-  template.querySelector(".chatContent").innerHTML = message;
-  document.getElementById("chatList").appendChild(template);
-}
-
-window.addEventListener('keydown', (event) => {
-  if (event.key == 'Enter') {
-    sendChat();
+  if (message.length) socket.emit("postChat", message);
+  if (++count == 10) {
+    document.getElementById("popupContainer").style.right = "20px";
+    count = 0;
   }
+}
+
+socket.on("updateChat", (Mcontent, sender = "Raphael") => {
+  let template = document.getElementById("newChat").content.cloneNode(true);
+  if (last != sender) template.querySelector(".chatSender").innerHTML = "Raphael";
+  template.querySelector(".chatContent").innerHTML = Mcontent;
+  let chat = document.getElementById("chatList")
+  chat.appendChild(template);
+  chat.scrollTop = chat.scrollHeight;
+  last = sender;
 });
+
+window.addEventListener('keyup', (event) => {
+  if (document.activeElement == input && event.key == 'Enter' && !event.shiftKey) sendChat();
+});
+
+function clickPopup() {
+  document.getElementById("popupContainer").style.right = "-100px";
+  window.open('bookings', '_blank');
+}
